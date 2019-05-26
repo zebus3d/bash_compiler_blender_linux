38bcb03c529163e75e9152e11091c947f2a35dd6
zebus3d <zebus3dream@gmail.com>
2019-05-26 20:48:18 +0200

    comillas simples

--- compile_in_linux.sh
+++ compile_in_linux.sh
@@ -22,43 +22,43 @@
 #############################################
 
 # el directorio donde trabajaremos:
-MAINDIR="$HOME/buildingBlender"
-# TARGETBRANCH="master"
-TARGETBRANCH="fluid-mantaflow"
+MAINDIR='$HOME/buildingBlender'
+# TARGETBRANCH='master'
+TARGETBRANCH='fluid-mantaflow'
 
-if [ ! -d "$MAINDIR" ]; then
+if [ ! -d '$MAINDIR' ]; then
     mkdir -p $MAINDIR
 else
-    echo "$MAINDIR ya existe no se hace nada"
+    echo '$MAINDIR ya existe no se hace nada'
 fi
 
 # Dependencias basicas:
-echo "######### DEPENDENCIAS BASICAS #########"
+echo '######### DEPENDENCIAS BASICAS #########'
 sudo apt install git build-essential cmake-gui
 
 # comprobando si existen los directorios
 # si no existen los creo:
-echo "######### Creando directorios #########"
-if [ ! -d "$MAINDIR/blender-git" ]; then
+echo '######### Creando directorios #########'
+if [ ! -d '$MAINDIR/blender-git' ]; then
     mkdir $MAINDIR/blender-git
 else
-    echo "$MAINDIR/blender-git ya existe no se hace nada"
+    echo '$MAINDIR/blender-git ya existe no se hace nada'
 fi
-if [ ! -d "$MAINDIR/2.80" ]; then
+if [ ! -d '$MAINDIR/2.80' ]; then
     mkdir $MAINDIR/2.80
 else
-    echo "$MAINDIR/2.80 ya existe no se hace nada"
+    echo '$MAINDIR/2.80 ya existe no se hace nada'
 fi
-if [ ! -d "$MAINDIR/mantaflow" ]; then
+if [ ! -d '$MAINDIR/mantaflow' ]; then
     mkdir $MAINDIR/mantaflow
 else
-    echo "$MAINDIR/mantaflow ya existe no se hace nada"
+    echo '$MAINDIR/mantaflow ya existe no se hace nada'
 fi
 
 
-echo "######### clonando blender #########"
-CHKVOIDDIR=$(find $MAINDIR/blender-git/blender -maxdepth 0 -empty -exec echo "True" \;)
-if [ "$CHKVOIDDIR" == "True" ]; then
+echo '######### clonando blender #########'
+CHKVOIDDIR=$(find $MAINDIR/blender-git/blender -maxdepth 0 -empty -exec echo 'True' \;)
+if [ '$CHKVOIDDIR' == 'True' ]; then
     cd $MAINDIR/blender-git 
     git clone https://git.blender.org/blender.git
     cd blender
@@ -66,28 +66,28 @@ if [ "$CHKVOIDDIR" == "True" ]; then
     git submodule foreach git checkout master
     git submodule foreach git pull --rebase origin master
 else
-    echo "$MAINDIR/blender-git ya tiene cosas dentro, no se clonara nada."
+    echo '$MAINDIR/blender-git ya tiene cosas dentro, no se clonara nada.'
 fi
 
 # actualizando el repo:
-echo "######### actualizando el repo #########"
+echo '######### actualizando el repo #########'
 cd $MAINDIR/blender-git/blender
 git checkout $TARGETBRANCH
 make update
 
 # dependencias de blender:
-echo "######### instalando dependecias #########"
+echo '######### instalando dependecias #########'
 cd $MAINDIR/blender-git/
 ./blender/build_files/build_environment/install_deps.sh
 
 
-echo "cmake automatico o por gui? (Auto/gui)"
+echo 'cmake automatico o por gui? (Auto/gui)'
 read ask
-if [ ! -z "$ask" ] || [ "$ask" == "gui" ] || [ "$ask" == "Gui" ] || [ "$ask" == "GUI" ]; then
+if [ ! -z '$ask' ] || [ '$ask' == 'gui' ] || [ '$ask' == 'Gui' ] || [ '$ask' == 'GUI' ]; then
     # configurar con gui:
-    echo "######### configurando cmake con gui #########"
+    echo '######### configurando cmake con gui #########'
     
-    if [ "$TARGETBRANCH" == "fluid-mantaflow" ]; then
+    if [ '$TARGETBRANCH' == 'fluid-mantaflow' ]; then
         cd $MAINDIR/mantaflow
     else
         cd $MAINDIR/2.80
@@ -95,22 +95,22 @@ if [ ! -z "$ask" ] || [ "$ask" == "gui" ] || [ "$ask" == "Gui" ] || [ "$ask" == 
     cmake-gui ../blender-git/blender
 else
     # configurar sin gui:
-    echo "######### configurando cmake #########"
+    echo '######### configurando cmake #########'
     
-    if [ "$TARGETBRANCH" == "fluid-mantaflow" ]; then
-        echo "######### entrando en $MAINDIR/mantaflow #########"
+    if [ '$TARGETBRANCH' == 'fluid-mantaflow' ]; then
+        echo '######### entrando en $MAINDIR/mantaflow #########'
         cd $MAINDIR/mantaflow
     else
-        echo "######### entrando en $MAINDIR/2.80 #########"
+        echo '######### entrando en $MAINDIR/2.80 #########'
         cd $MAINDIR/2.80
     fi
     cmake ../blender-git/blender -WITH_STATIC_LIBS=ON -DWITH_CXX11=ON -DGUI=OFF -DWITH_FFTW3=ON -DWITH_MOD_OCEANSIM=ON -DWITH_ALEMBIC=ON -DWITH_INSTALL_PORTABLE=ON -DWITH_BUILDINFO=ON
 fi
 
 # compilando:
-echo "######### compilando #########"
+echo '######### compilando #########'
 
-if [ "$TARGETBRANCH" == "fluid-mantaflow" ]; then
+if [ '$TARGETBRANCH' == 'fluid-mantaflow' ]; then
     cd $MAINDIR/mantaflow
 else
     cd $MAINDIR/blender-git/2.8
@@ -132,4 +132,4 @@ make install
 
 
 # conocer las librerias necesarias para compartir la build:
-#objdump -x path/to/blender | grep "NEEDED"
+#objdump -x path/to/blender | grep 'NEEDED'
